cd7a49d389d8bdc68863818ded82faaa4135009b
Apnacollege <ajay91044@gmail.com>
2024-05-27 01:38:58 +0530

    a

--- Controller/Formdata.controller.js
+++ Controller/Formdata.controller.js
@@ -1,5 +1,44 @@
 let SignupdataCollection = require('../Model/Formdata.model');
 
+const path = require('path');
+const fs = require('fs');
+
+// let data=require('/Users/aj/Documents/NewRitsPod/src/Components/Pluggings/Assemble')
+
+// const dcCoollect=require(`C:\Users\aj\Documents\NewRitsPod\src\Components\Pluggings\Assemble.jsx`)
+
+
+let getStartService=async (req,res)=>
+    {
+        try {
+            res.json({ error: false, message: "Started successfully", data: addSignupData });
+          
+        } catch (err) {
+            console.error("Error adding data:", err);
+            res.status(500).json({ error: true, message: "Failed to add data" });
+        }
+    }
+let getCompleteService=async (req,res)=>
+    {
+        try {
+            res.json({ error: false, message: "Completed Succesfully", data: addSignupData });
+
+        } catch (err) {
+            console.error("Error adding data:", err);
+            res.status(500).json({ error: true, message: "Failed to add data" });
+        }
+    }
+let getSignOffService=async (req,res)=>
+    {
+        try {
+            res.json({ error: false, message: "SignedOff successfully", data: addSignupData });
+
+        } catch (err) {
+            console.error("Error adding data:", err);
+            res.status(500).json({ error: true, message: "Failed to add data" });
+        }
+    }
+
 // Add a new signup data
 let addSignupData = async (req, res) => {
     try {
@@ -42,7 +81,8 @@ let getdata = async (req, res) => {
         let getdatas = await SignupdataCollection.find();
         let totalDocument = await SignupdataCollection.find().count();
 
-        res.json({ error: false, dataFromMongo: getdatas,totalDocument:totalDocument });
+        // res.json({ error: false, dataFromMongo: getdatas,totalDocument:totalDocument });
+        res.json({ error: false, dataFromMongo: 'http/Users/aj/Documents/NewRitsPod/src/Components/Pluggings/Assemble',totalDocument:totalDocument });
     } catch (err) {
         console.error("Error fetching data:", err);
         res.status(500).json({ error: true, message: "Failed to fetch data" });
@@ -66,4 +106,7 @@ let deleteSingleData = async (req, res) => {
 }
 
 
-module.exports = { addSignupData, getdata, deleteSingleData, rulesdata};
+
+
+module.exports = { addSignupData, getdata, deleteSingleData, rulesdata,getStartService,getCompleteService,getSignOffService};
+
--- Routes/Formdata.routes.js
+++ Routes/Formdata.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addSignupData, getdata, deleteSingleData,rulesdata } = require('../Controller/Formdata.controller');
+const { addSignupData, getdata, deleteSingleData,rulesdata,getStartService,getCompleteService ,getSignOffService} = require('../Controller/Formdata.controller');
 
 let router = express.Router();
 
@@ -8,5 +8,8 @@ router.get('/getdata', getdata);
 // Correct the route definition to include the ID parameter
 router.delete('/deleteSingleData/:id', deleteSingleData);
 router.post('/rulesdata',rulesdata)
+router.get('/getStartService',getStartService)
+router.get('/getCompleteService',getCompleteService)
+router.get('/getSignOffService',getSignOffService)
 
 module.exports = router;
